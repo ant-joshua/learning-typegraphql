f6f31e0970498dba2fa10124b310c5d6794266d7
ant_joshua.hm24 <antonius.joshua@homecare24.id>
2021-01-17 01:16:18 +0700

    initial setup

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Learning Graphql
+
+This is my repo for learning graphql and typegraphql from ben awad playlist
+
+# Motivation
+
+Understand how graphql works with typescript and nodejs
+
+# Tech / Framework Used
+
+- graphql
+- typegraphql
+- typeORM
+- express
+- pg (postgreSQL)
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import * as Express from "express";
+import { buildSchema, Query, Resolver } from "type-graphql";
+
+@Resolver()
+class HelloResolver {
+  // private recipesCollection: Recipe[] = [];
+
+  @Query(() => String, { name: "helloWorld", nullable: true })
+  async helloWorld() {
+    // fake async in this example
+    return "hello world";
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = Express();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started at localhost:4000");
+  });
+};
+
+main();
